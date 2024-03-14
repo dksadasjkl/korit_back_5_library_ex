17469bfef6a087ecd4c3dbab436a1a77622a501d
code1218 <skjil1218@gmail.com>
2024-03-14 12:49:12 +0900

    validation

--- src/main/java/com/study/library/controller/AuthController.java
+++ src/main/java/com/study/library/controller/AuthController.java
@@ -2,16 +2,44 @@ package com.study.library.controller;
 
 import com.study.library.aop.annotation.ParamsPrintAspect;
 import com.study.library.dto.SignupReqDto;
+import com.study.library.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @ParamsPrintAspect
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupReqDto signupReqDto) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) {
+        if(authService.isDuplicatedByUsername(signupReqDto.getUsername())) {
+            ObjectError objectError = new FieldError("username", "username", "이미 존재하는 사용자이름입니다");
+            bindingResult.addError(objectError);
+        }
+
+        if(bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError fieldError : fieldErrors) {
+                String fieldName = fieldError.getField();   // DTO 변수명
+                String message = fieldError.getDefaultMessage();    // 메세지내용
+                errorMap.put(fieldName, message);
+            }
+            return ResponseEntity.badRequest().body(errorMap);
+        }
 
         return ResponseEntity.ok(null);
     }
--- src/main/java/com/study/library/dto/SignupReqDto.java
+++ src/main/java/com/study/library/dto/SignupReqDto.java
@@ -2,10 +2,18 @@ package com.study.library.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class SignupReqDto {
+    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{2,10}+$", message = "영문 숫자 조합 6~10자리여야 합니다")
     private String username;
-    private String password;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{7,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다")
+    private String password;    // 1q2w3e4r!
+    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]{1,}$", message = "한글이어야 합니다")
     private String name;
+    @Email(regexp = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]+$")
     private String email;
 }
